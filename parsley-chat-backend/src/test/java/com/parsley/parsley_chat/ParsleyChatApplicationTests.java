d6f71fe1c095f7cb4f1547ed66f84b6a8261cb79
Zeus Huxtable <zeushuxtable@gmail.com>
2025-10-27 16:51:29 +0000

    initial commit

---
+++ parsley-chat-backend/src/test/java/com/parsley/parsley_chat/ParsleyChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parsley.parsley_chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParsleyChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
